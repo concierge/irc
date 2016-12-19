52071e5aedae82d68ed74143b75c8b8aecf295ce
Jay Harris <jay.harris@outlook.co.nz>
2016-12-19 20:36:09 +1300

    Adds basic IRC intergration

---
+++ irc.js
@@ -0,0 +1,58 @@
+var irc = require('irc'),
+    client,
+    api,
+    messageCallback;
+    
+exports.getApi = function() {
+    return api;
+};
+
+exports.start = function (callback) {
+    messageCallback = callback;
+
+    if (!exports.config.server) {
+        throw new Error("'server' must be set in config!");
+    }
+
+    if (!exports.config.nick) {
+        throw new Error("'nick' must be set in config")
+    }
+
+    client = new irc.Client(exports.config.server,
+                            exports.config.nick,
+                            {
+                                channels: exports.config.channels || []
+                            });
+
+    client.addListener('message', function (from, to, message) {
+        let event = shim.createEvent(to, from, from, message);
+        messageCallback(api, event);
+    });
+
+    //Dump errors to the console
+    client.addListener('error', function(message) {
+        console.log('error: ', message);
+    });
+
+    class IRCIntegration extends shim {
+        sendMessage(message, thread) {
+            client.say(thread, message);
+        }
+
+        getUsers(thread) {
+            let obj = {};
+            if (thread == exports.config.threadId) {
+                obj[exports.config.senderId] = {
+                    name: exports.config.senderName
+                }
+            }
+            return obj;
+        }
+    }
+
+    api = new IRCIntegration(exports.config.commandPrefix);
+};
+
+exports.stop = function () {
+    client.stop();
+};
